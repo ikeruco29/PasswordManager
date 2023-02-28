7d2d432c9a257cbc8b015913bea57f78f39b1398
ikeruco29 <ikeruco29@gmail.com>
2023-02-28 19:50:12 +0100

    Project initialized

---
+++ passwordmng-api/src/main/java/com/ikeruco29/passwordmngapi/PasswordmngApiApplication.java
@@ -0,0 +1,13 @@
+package com.ikeruco29.passwordmngapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordmngApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordmngApiApplication.class, args);
+	}
+
+}
---
+++ passwordmng-api/src/test/java/com/ikeruco29/passwordmngapi/PasswordmngApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikeruco29.passwordmngapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordmngApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
